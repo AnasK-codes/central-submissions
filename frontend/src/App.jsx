d52a9a56dae92eace96c2b82d835bbcb5d87382c
AnasK-codes <anaskhan9826921110@gmail.com>
2026-03-28 22:07:00 +0530

    fix: update App.jsx import to use lowercase components (Vercel build fix)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css'
 import { AuthProvider } from './context/AuthContext'
-import ProtectedRoute from './Components/ProtectedRoute'
+import ProtectedRoute from './components/ProtectedRoute'
 import LoginPage from './pages/LoginPage'
 import SignupPage from './pages/SignupPage'
 import InterviewConfig from './pages/InterviewConfig'
